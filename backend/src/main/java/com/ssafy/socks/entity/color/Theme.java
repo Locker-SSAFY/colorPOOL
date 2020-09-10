29538790a583ce93dd095a7f3ff1f38c24c9fa30
junhok82 <junhok82@gmail.com>
2020-09-10 14:11:51 +0900

    feat : Create Theme Entity | S03P22A303-36

--- backend/src/main/java/com/ssafy/socks/entity/color/Theme.java
+++ backend/src/main/java/com/ssafy/socks/entity/color/Theme.java
@@ -1,2 +1,36 @@
-package com.ssafy.socks.entity.color;public class Theme {
+package com.ssafy.socks.entity.color;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity @Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor
+@Table(name = "THEME")
+public class Theme {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "THEME_ID", nullable = false)
+	private Long id;
+
+	@Embedded
+	private ThemeColor themeColor;
+
+	@ManyToOne
+	@JoinColumn(name = "SELECTED_COLOR_ID")
+	private SelectedColor selectedColor;
 }
