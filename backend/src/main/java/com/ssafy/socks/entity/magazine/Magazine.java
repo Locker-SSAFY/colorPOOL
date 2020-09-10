a1051f0706b477243c6e14e5a2c60845f27f6b6c
junhok82 <junhok82@gmail.com>
2020-09-10 15:45:46 +0900

    feat : Create Magazine Entity | S03P22A303-37

--- backend/src/main/java/com/ssafy/socks/entity/magazine/Magazine.java
+++ backend/src/main/java/com/ssafy/socks/entity/magazine/Magazine.java
@@ -1,18 +1,24 @@
-package com.ssafy.socks.entity.color;
+package com.ssafy.socks.entity.magazine;
+
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.ssafy.socks.entity.user.User;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-@Entity @Setter @Getter @ToString @NoArgsConstructor
+@Entity @Setter @Getter @NoArgsConstructor
 @Table(name = "MAGAZINE")
 public class Magazine {
 	@Id
@@ -20,5 +26,13 @@ public class Magazine {
 	@Column(name = "MAGAZINE_ID", nullable = false)
 	private Long id;
 
-	
+	@ManyToOne
+	@JoinColumn(name = "USER_ID", nullable = false)
+	private User user;
+
+	@OneToMany(mappedBy = "magazine")
+	private List<Contents> contents;
+
+	@OneToMany(mappedBy = "magazine")
+	private List<Likes> likes;
 }
