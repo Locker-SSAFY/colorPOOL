c5dc8e56fc7620f3caa364709952d276a91d21b6
junhok82 <junhok82@gmail.com>
2020-09-10 14:09:47 +0900

    feat : Create Any color Entity | S03P22A303-31

--- backend/src/main/java/com/ssafy/socks/entity/color/AnyColor.java
+++ backend/src/main/java/com/ssafy/socks/entity/color/AnyColor.java
@@ -1,10 +1,12 @@
 package com.ssafy.socks.entity.color;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +14,14 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity @Setter @Getter @NoArgsConstructor @ToString
-public class themeColor {
+@Table(name = "ANY_COLOR")
+public class AnyColor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "THEME_COLOR_ID")
+	@Column(name = "ANY_COLOR_ID", nullable = false)
 	private Long id;
 
+	@Embedded
+	private Color color;
 
 }
