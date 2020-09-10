598592662710dfe06bd55ddf3f75f46e74504048
junhok82 <junhok82@gmail.com>
2020-09-10 14:10:36 +0900

    feat : Create SelectedColor Entity | S03P22A303-35

--- backend/src/main/java/com/ssafy/socks/entity/color/SelectedColor.java
+++ backend/src/main/java/com/ssafy/socks/entity/color/SelectedColor.java
@@ -1,2 +1,37 @@
-package com.ssafy.socks.entity.color;public class SelectedColor {
+package com.ssafy.socks.entity.color;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString @Entity @Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Table(name = "SELECTED_COLOR")
+public class SelectedColor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "SELECTED_COLOR_ID", nullable = false)
+	private Long id;
+
+	@Embedded
+	private Color color;
+
+	@Column(name = "IMG", nullable = false)
+	private String img;
+
+	@OneToMany(mappedBy = "selectedColor")
+	private List<Theme> themes;
 }
