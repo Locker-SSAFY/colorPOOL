4993e442984c4091e770101d45433ca12b3f4c3e
junhok82 <junhok82@gmail.com>
2020-09-14 18:06:21 +0900

    feat : Create User Model for json

--- backend/src/main/java/com/ssafy/socks/model/user/UserResult.java
+++ backend/src/main/java/com/ssafy/socks/model/user/UserResult.java
@@ -1,4 +1,14 @@
 package com.ssafy.socks.model.user;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter @AllArgsConstructor @NoArgsConstructor
 public class UserResult {
+	private String email;
+	private String password;
 }
