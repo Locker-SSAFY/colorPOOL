2534612599c55c761dc0a06fe9ddf1a9db17f225
junhok82 <junhok82@gmail.com>
2020-09-14 14:35:05 +0900

    feature : Create Custom User Duplicated Exception

--- backend/src/main/java/com/ssafy/socks/advice/ExceptionAdvice.java
+++ backend/src/main/java/com/ssafy/socks/advice/ExceptionAdvice.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.ssafy.socks.advice.exception.CUserDuplicatedException;
 import com.ssafy.socks.advice.exception.CUserNotFoundException;
 import com.ssafy.socks.model.response.CommonResult;
 import com.ssafy.socks.service.ResponseService;
@@ -24,15 +26,19 @@ public class ExceptionAdvice {
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-		// 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
 		return responseService.getFailResult(Integer.parseInt(getMessage("unKnown.code")), getMessage("unKnown.msg"));
 	}
 
 	@ExceptionHandler(CUserNotFoundException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	protected CommonResult userNotFoundException(HttpServletRequest request, CUserNotFoundException e) {
-		// 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
 		return responseService.getFailResult(Integer.parseInt(getMessage("userNotFound.code")), getMessage("userNotFound.msg"));
+	}
+
+	@ExceptionHandler(CUserDuplicatedException.class)
+	@ResponseStatus(HttpStatus.CONFLICT) // 409
+	protected CommonResult userDuplicatedException(HttpServletRequest request, CUserDuplicatedException e) {
+		return responseService.getFailResult(Integer.parseInt(getMessage("userDuplicated.code")), getMessage("userDuplicated.msg"));
 	}
 
 	// code정보에 해당하는 메시지를 조회합니다.
