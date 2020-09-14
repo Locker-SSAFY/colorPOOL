3a9aa3bdc58a546fcbf114bc025fb1ae902b415b
junhok82 <junhok82@gmail.com>
2020-09-14 14:31:00 +0900

    fix : Modify gitignore

---
+++ backend/src/main/java/com/ssafy/socks/advice/exception/CUserDuplicatedException.java
@@ -0,0 +1,2 @@
+package com.ssafy.socks.advice.exception;public class CUserDuplicatedException {
+}
--- backend/src/main/java/com/ssafy/socks/repository/user/UserJpaRepository.java
+++ backend/src/main/java/com/ssafy/socks/repository/user/UserJpaRepository.java
@@ -1,12 +1,2 @@
-package com.ssafy.socks.repository.user;
-
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.socks.entity.user.User;
-
-public interface UserJpaRepository extends JpaRepository<User,Integer> {
-	Optional<User> findById(Long id);
+package com.ssafy.socks.repository.user;public interface UserJpaRepository {
 }
