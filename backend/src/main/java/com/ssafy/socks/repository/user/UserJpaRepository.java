b14edd5cfdc98aa2ee001cfb5816003c005751dc
junhok82 <junhok82@gmail.com>
2020-09-14 14:35:54 +0900

    feat : Update UserJpaRepository

--- backend/src/main/java/com/ssafy/socks/repository/user/UserJpaRepository.java
+++ backend/src/main/java/com/ssafy/socks/repository/user/UserJpaRepository.java
@@ -1,2 +1,12 @@
-package com.ssafy.socks.repository.user;public interface UserJpaRepository {
+package com.ssafy.socks.repository.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.socks.entity.user.User;
+
+public interface UserJpaRepository extends JpaRepository<User,Long> {
+	Optional<User> findById(Long id);
+	Optional<User> findByEmail(String email);
 }
