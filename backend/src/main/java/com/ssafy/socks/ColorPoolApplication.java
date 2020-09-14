49a3e78ade8ec9d151cecd02c6e417534af083c3
junhok82 <junhok82@gmail.com>
2020-09-14 14:31:59 +0900

    feature : Create Password Encoder for security

--- backend/src/main/java/com/ssafy/socks/ColorPoolApplication.java
+++ backend/src/main/java/com/ssafy/socks/ColorPoolApplication.java
@@ -2,6 +2,9 @@ package com.ssafy.socks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ColorPoolApplication {
@@ -10,4 +13,8 @@ public class ColorPoolApplication {
 		SpringApplication.run(ColorPoolApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
