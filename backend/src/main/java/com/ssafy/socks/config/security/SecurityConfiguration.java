8ef456e502d2d92c17e8b27642381191a5a188f4
junhok82 <junhok82@gmail.com>
2020-09-15 02:16:07 +0900

    chore : Modify Authentication for admin

--- backend/src/main/java/com/ssafy/socks/config/security/SecurityConfiguration.java
+++ backend/src/main/java/com/ssafy/socks/config/security/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.and()
 			.authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
 			.antMatchers("/*/signin", "/*/signup").permitAll() // 가입 및 인증 주소는 누구나 접근가능
-			.antMatchers( "api/**").hasRole("ADMIN") // api 시작하는 리소스는 관리자만 접근가능
+			.antMatchers( "/*/users").hasRole("ADMIN") // api 시작하는 리소스는 관리자만 접근가능
 			.anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
 			.and()
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // jwt token 필터를 id/password 인증 필터 전에 넣는다
