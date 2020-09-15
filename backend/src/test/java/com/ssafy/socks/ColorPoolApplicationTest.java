f6177728949b4768e48cc6c02b8ee84f317f1545
junhok82 <junhok82@gmail.com>
2020-09-15 19:02:26 +0900

    chore : Create Bean for RestTemplate

--- backend/src/test/java/com/ssafy/socks/ColorPoolApplicationTest.java
+++ backend/src/test/java/com/ssafy/socks/ColorPoolApplicationTest.java
@@ -4,11 +4,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class ColorPoolApplicationTest {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
