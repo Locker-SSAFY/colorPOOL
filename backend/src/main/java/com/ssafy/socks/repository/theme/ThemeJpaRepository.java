1eedbe373ce62b74c94f93b11673ffcb80b450cf
junhok82 <junhok82@gmail.com>
2020-10-03 23:48:14 +0900

    feat : Create theme related repository

--- backend/src/main/java/com/ssafy/socks/repository/theme/ThemeJpaRepository.java
+++ backend/src/main/java/com/ssafy/socks/repository/theme/ThemeJpaRepository.java
@@ -1,2 +1,8 @@
-package com.ssafy.socks.repository.theme;public interface ThemeJpaRepository {
+package com.ssafy.socks.repository.theme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.socks.entity.color.Theme;
+
+public interface ThemeJpaRepository extends JpaRepository<Theme,Long> {
 }
